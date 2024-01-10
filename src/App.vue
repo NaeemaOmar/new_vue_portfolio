3ae3310a38f4296337605984b755b77a26adb5f3
NaeemaOmar <naeemaomar19@gmail.com>
2024-01-10 09:14:26 +0200

    created a footer with some styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar/>
   <router-view/>
+  <Footer/>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default{
+  components:{
+    NavBar,
+    Footer
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,9 +25,6 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
 
 nav a {
   font-weight: bold;
