e3a52833deefb519ac42fd2cd7fdedfa6b0de086
NaeemaOmar <naeemaomar19@gmail.com>
2024-03-25 15:04:30 +0200

    updated animation

--- src/views/GoldHomePg.vue
+++ src/views/GoldHomePg.vue
@@ -11,10 +11,20 @@
 .golden-circle {
   width: 100px; /* Adjust size as needed */
   height: 100px; /* Adjust size as needed */
-  background: linear-gradient(45deg, #ffd700, #e5c100); /* Golden gradient */
+  background: linear-gradient(45deg, #ffd700, #e5c100, #d8d8d8, #ffbf00, #f5bf03 ); /* Golden gradient */
   border-radius: 50%; /* Makes the element circular */
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); /* Adds a shadow for depth */
-  transform: rotate(-45deg); /* Gives a metallic effect */
+  animation: rotation 10s timing-function 0 iteration-count direction fill-mode;
+  animation-name: rotation;
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes rotation{
+    0%{
+        transform: rotate(360deg); 
+    }
+
 }
 
 .blackBg{
